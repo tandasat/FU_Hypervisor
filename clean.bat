f572e9df78d10a2d001735244d8a0f7117210c34
tandasat <tanda.sat@gmail.com>
2016-07-12 06:54:26 -0700

    add basic files

---
+++ clean.bat
@@ -0,0 +1,16 @@
+@echo off
+del *.sdf
+
+del *.VC.db
+del /s *.aps
+del /a:h *.suo
+rmdir /s /q .vs
+rmdir /s /q ipch
+rmdir /s /q x64
+rmdir /s /q Debug
+rmdir /s /q Release
+rmdir /s /q FU_Hypervisor\x64
+rmdir /s /q FU_Hypervisor\Debug
+rmdir /s /q FU_Hypervisor\Release
+cd HyperPlatform
+clean.bat
